3262a8d6a9faabb461b7e2d36b8db428f5e60807
Chenxu Zhao <zhaochenxu001@gmail.com>
2017-01-31 17:00:20 +1100

    First Commit

---
+++ spec/caniuse-css-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CaniuseCssView from '../lib/caniuse-css-view';
+
+describe('CaniuseCssView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
